f9795e9dba56952cbd9ff13ea7b1243ff7b97344
Guyoguyes <g.abuba43@gmail.com>
2020-05-27 12:09:49 +0300

    TODO second test pass

--- src/main/java/models/TODO.java
+++ src/main/java/models/TODO.java
@@ -1,7 +1,26 @@
 package models;
 
-public class TODO {
-    public TODO(String task){
+import java.util.ArrayList;
 
+public class TODO {
+
+    private String task;
+    private String description;
+    private String completeDate ;
+    private  static ArrayList<Object> mInstances = new ArrayList<>();
+
+    public TODO(String task, String description, String completeDate){
+        this.task = task;
+        this.description = description;
+        this.completeDate = completeDate;
+        mInstances.add(this);
+    }
+
+    public static ArrayList<Object> getAll(){
+        return  mInstances;
+    }
+
+    public static void clearAllTODO(){
+        mInstances.clear();
     }
 }
--- src/test/java/models/TODOTest.java
+++ src/test/java/models/TODOTest.java
@@ -1,5 +1,6 @@
 package models;
 
+import com.sun.tools.javac.comp.Todo;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,11 +15,20 @@ public class TODOTest {
 
     @After
     public void tearDown() throws Exception {
+        TODO.clearAllTODO();
     }
 
     @Test
     public void todo_createInstancesOfObject_true(){
-        TODO todo = new TODO("Do tests");
+        TODO todo = new TODO("Do tests", "Make sure they run", "12/12/2020");
         assertEquals(true, todo instanceof TODO);
+    }
+
+    @Test
+    public void todo_allTodoAreCorrectlyReturned_true(){
+        TODO todo = new TODO("Do test", "Make user they pass", "03/12/2020");
+        TODO otherTodo = new TODO("code", "Bluecollar", "03/12/2020");
+
+        assertEquals(2, TODO.getAll().size());
     }
 }
